650939dfce2b496bae5eeb0dddaef66159166bec
Sean Huber <sean@onmyblock.com>
2015-02-27 15:00:20 -0800

    Add spec for `SubDiff`

---
+++ spec/sub_diff_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/sub_diff'
+
+RSpec.describe SubDiff do
+  subject { 'example' }
+
+  let(:differ) { double 'differ', diff: 'diffed' }
+
+  it { is_expected.to be_a described_class }
+
+  describe '#sub_diff' do
+    it 'should delegate to Sub#diff' do
+      expect(described_class::Sub).to receive(:new)
+                                      .with(subject)
+                                      .and_return differ
+
+      expect(subject.sub_diff).to eq 'diffed'
+    end
+  end
+
+  describe '#gsub_diff' do
+    it 'should delegate to Gsub#diff' do
+      expect(described_class::Gsub).to receive(:new)
+                                       .with(subject)
+                                       .and_return differ
+
+      expect(subject.gsub_diff).to eq 'diffed'
+    end
+  end
+end
