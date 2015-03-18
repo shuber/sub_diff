777233b247ed5e5a4db0d7ab4e5c30cb6ab1afa8
Sean Huber <github@shuber.io>
2015-03-18 15:32:06 -0700

    Don't chain rspec matchers with newlines

--- spec/sub_diff_spec.rb
+++ spec/sub_diff_spec.rb
@@ -9,20 +9,14 @@ RSpec.describe SubDiff do
 
   describe '#sub_diff' do
     it 'should delegate to Sub#diff' do
-      expect(described_class::Sub).to receive(:new)
-                                      .with(subject)
-                                      .and_return(differ)
-
+      expect(described_class::Sub).to receive(:new).with(subject).and_return(differ)
       expect(subject.sub_diff).to eq('diffed')
     end
   end
 
   describe '#gsub_diff' do
     it 'should delegate to Gsub#diff' do
-      expect(described_class::Gsub).to receive(:new)
-                                       .with(subject)
-                                       .and_return(differ)
-
+      expect(described_class::Gsub).to receive(:new).with(subject).and_return(differ)
       expect(subject.gsub_diff).to eq('diffed')
     end
   end
