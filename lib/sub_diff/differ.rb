020a16a13af9040128fb8ebb9010b60824014b97
Sean Huber <github@shuber.io>
2015-03-26 01:03:15 -0700

    Delegate Differ#string to Differ#collection

--- lib/sub_diff/differ.rb
+++ lib/sub_diff/differ.rb
@@ -1,22 +1,22 @@
+require 'forwardable'
+
 module SubDiff
   class Differ < Struct.new(:collection, :type)
+    extend Forwardable
+
+    def_delegators :collection, :string
+
     def diff(*args)
       # Ruby 1.8.7 does not support additional args after * (splat)
       block = args.pop
 
-      differ.call(args.first) do |match|
+      string.send(type, args.first) do |match|
         diff = { :match => match, :prefix => $`, :suffix => $' }
         diff[:replacement] = match.sub(*args, &block)
         yield(collection, diff)
       end
 
       collection
-    end
-
-    private
-
-    def differ
-      collection.string.method(type)
     end
   end
 end
