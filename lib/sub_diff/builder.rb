6df3ef7d58bf3441e19392938cbe334519672e6b
Sean Huber <github@shuber.io>
2015-06-11 16:14:13 -0700

    Remove inline documentation for now

--- lib/sub_diff/builder.rb
+++ lib/sub_diff/builder.rb
@@ -1,21 +1,12 @@
 module SubDiff
-  # Performs a `Sub` or `Gsub` replacement and returns the
-  # resulting {Collection} of {Diff} objects.
-  #
-  # Used internally by {String#sub_diff} and {String#gsub_diff}.
-  #
-  # @api private
   class Builder
     attr_reader :string, :type
 
-    # @param string [String] the string to perform a replacement on.
-    # @param type [Symbol] the type of replacement - :sub or :gsub.
     def initialize(string, type)
       @string = string
       @type = type
     end
 
-    # @return [Collection<Diff>] the resulting diffs from the replacement.
     def diff(*args, &block)
       build_diff_collection do
         adapter.diff(*args, &block)
