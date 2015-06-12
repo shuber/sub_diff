e1ab765c174196557d68439431add2619a78ca2a
Sean Huber <github@shuber.io>
2015-06-12 16:53:12 -0700

    Remove spec_helper code for 1.8.7 support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
-begin
-  if ENV['CODECLIMATE_REPO_TOKEN']
-    require 'codeclimate-test-reporter'
-    CodeClimate::TestReporter.start
-  else
-    require 'simplecov'
-    SimpleCov.start { add_filter('/vendor/bundle/') }
-  end
-rescue LoadError
-  # Ignore when testing with Ruby 1.8.7
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start { add_filter('/vendor/bundle/') }
 end
 
 require File.expand_path('../../lib/sub_diff', __FILE__)
