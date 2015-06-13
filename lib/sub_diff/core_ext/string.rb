9dd404db564a183c05d31b7288b286d1ce4bf581
Sean Huber <github@shuber.io>
2015-06-12 17:39:45 -0700

    Add inline documentation for sub_diff and gsub_diff

--- lib/sub_diff/core_ext/string.rb
+++ lib/sub_diff/core_ext/string.rb
@@ -1,10 +1,18 @@
 module SubDiff
   module CoreExt
     module String
+      # Behaves just like {String#sub} but wraps the returned replacement
+      # string in an enumerable {Collection} of {Diff} objects.
+      #
+      # See http://ruby-doc.org/core-2.2.0/String.html#method-i-sub
       def sub_diff(*args, &block)
         Builder.new(self, :sub).diff(*args, &block)
       end
 
+      # Behaves just like {String#gsub} but wraps the returned replacement
+      # string in an enumerable {Collection} of {Diff} objects.
+      #
+      # See http://ruby-doc.org/core-2.2.0/String.html#method-i-gsub
       def gsub_diff(*args, &block)
         Builder.new(self, :gsub).diff(*args, &block)
       end
